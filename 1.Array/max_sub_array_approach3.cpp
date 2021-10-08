ee10b4c6d35fa34e91a7196dbb1807e5b99d1b6d
Raghav Agarwal <raghavagrawal406@gmail.com>
2021-10-08 09:15:01 +0530

    modified kadane algo

--- 1.Array/max_sub_array_approach3.cpp
+++ 1.Array/max_sub_array_approach3.cpp
@@ -1,18 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-
-	int n;
-	cin>>n;
-	int a[1000];
-	for(int i=0; i<n; i++){
-		cin>>a[i];
-	}
-
+int modifiedkadane(int *a,int n){
+    int sum = INT_MIN; // largest sum
+    int currsum = 0;   // current running sum
+    int maxelem = INT_MIN; // to find max ele (needed for all negative case ones)
+   
+    for(int i=0;i<n;i++){
+        currsum += a[i];
+        if(currsum < 0){
+            currsum = 0;
+        }
+        sum = max(sum,currsum); // till here code is same as kadane algo code
+        if(maxelem < a[i]){     // to calculate max ele (needed for all -ve case)
+            maxelem = a[i];
+        }
+    }
+    return (maxelem < 0 ? maxelem : sum);//if all are -ve return maxelem, else sum
+}
+int kadanesAlgo(int*a, int n){
+	
 	//Kadane's Algorithm for maximum subarray sum
+	//this algo will not work if all the elements of the array are negative
 	int curr_sum = 0, max_sum = 0;
-
+	
 	for(int i=0; i<n; i++){
 		curr_sum += a[i];
 		if(curr_sum < 0){
@@ -21,7 +32,21 @@ int main(){
 		if(curr_sum > max_sum )
 			max_sum = curr_sum;
 	}
-	cout<<max_sum;
+	return max_sum;
+}
 
+int main(){
+
+	int n;
+	cin>>n;
+	int a[1000];
+	
+	for(int i=0; i<n; i++){
+		cin>>a[i];
+	}
+	int ans = modifiedkadane(a,n);
+	cout<<ans<<endl;
+
+	
 	return 0;
 }
--- 1.Array/practice.cpp
+++ 1.Array/practice.cpp
@@ -1,44 +1,23 @@
 #include<iostream>
+#include<climits>
+#include<vector>
 using namespace std;
-/*
-O(N2) solution for max subarrays sum
-a =         1 2 3 4 5
-cum_sum =   1 3 6 10 15
-cum_sum[i] = a[i]+cum_sum[i-1] for i>0
-Now for sum between any pair of i and j will be given by
-sum(i,j) = cum_sum[j] - cum_sum[i-1].
-Here, we will need only two nested loops for finding the possible pairs i,j
-*/
-int main(){
 
-    int a[] = {-4,1,3,-2,6,2,-1,-4,-7};
-    int n = sizeof(a)/sizeof(int);
-    int max_sum = 0;
-    int cur_sum = 0;
-    int left = -1;
-    int right = -1;
-    int cum_sum[100];
-    cum_sum[0] = a[0];
-    //forming the cum_sum array
-    for(int i = 1; i<n; i++){
-        cum_sum[i] = a[i] + cum_sum[i-1];
-    }
-    for(int i =0; i<n; i++){
-        for(int j = i; j<n; j++){
-            cur_sum = cum_sum[j] - cum_sum[i-1];
-            if(cur_sum>max_sum){
-                max_sum = cur_sum;
-                left = i;
-                right = j;
-            }
+int max_subarray_sum(vector<int> arr) {
+    int cs= 0; 
+    int ms = 0;
+    for(int x : arr){
+        cs += x;
+        if(cs<0){
+            cs = 0;
         }
+        ms = max(cs,ms);
     }
-    cout<<"Maximum sum ="<<max_sum<<endl;
-    cout<<"subarray that gives max sum is : "<<endl;
-    for(int i = left; i<=right; i++){
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    
+    return ms;
+}
+int main(){
+    vector<int> a{-4,1,3,-2,6,2,-1,-4,-7};
+    int ans = max_subarray_sum(a);
+    cout<<ans<<endl;
     return 0;
 }
