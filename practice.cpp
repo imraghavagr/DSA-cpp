cfbf5c0ce26a8f67d219f3d6007d53c2a49b5332
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-03-18 20:34:05 +0530

    modified files

--- practice.cpp
+++ practice.cpp
@@ -1,72 +1,41 @@
+//nearest greater to the right
 #include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
 using namespace std;
-/*
-
-		100			200			300			400
-h 100	1 200		2 300		3 400		4 NULL	 
-
-			10000
-h = 10000	5	Null
-
-				10			10000
-h = 10000		4 NULL		5  NULL
-
-				10			10000
-h = 10	->		4 10000	->	5  NULL
-
-*/
-class node{
-public:
-	int data;
-	node* next;
-	node(int d)	{
-		data = d;
-		next = NULL;
-	}
-};
-void insertAtMiddle(int d, node*&head, int p){
+vector<int> solve(vector<int> v){
 	
-}
-void insertAtHead(int d, node*&head){
-	if(head == NULL){
-		head = new node(d);
-		return;
-	}
-	node* n = new node(d);
-	n->next = head;
-	head = n;
-}
+	vector<int> ans;
+	stack<int> s;
 
-void insertAtTail(int d, node*&head){
-	if(head == NULL){
-		head = new node(d);
-		return;
+	for(int i = v.size()-1; i>=0; i--){
+		if(s.empty()){
+			ans.push_back(-1);
+		}
+		else if(v[i]<s.top()){
+			ans.push_back(s.top());
+		}
+		else if(v[i] >= s.top()){
+			while(!s.empty() and v[i] >= s.top()){
+				s.pop();
+			}
+			if(s.empty()){
+				ans.push_back(-1);
+			}
+			else{
+				ans.push_back(s.top());
+			}
+		}
+		s.push(v[i]);
 	}
-	node*temp = head;
-	while(temp->next != NULL){
-		temp = temp->next;
-	}
-	node* n = new node(d);
-	temp->next = n;
-}
-void printLL(node*head){
-	while(head != NULL){
-		cout<<head->data<<"->";
-		head = head->next;
-	}
-	cout<<endl;
+	reverse(ans.begin(), ans.end());
+	return ans;
 }
 int main(){
-	
-	node*head = NULL;
-	insertAtTail(6,head);
-	insertAtHead(5,head);
-	insertAtHead(4,head);
-	insertAtHead(3,head);
-	insertAtHead(2,head);
-	insertAtHead(1,head);
-	insertAtTail(7,head);
-	printLL(head);
-	printLL(head);
+	vector<int> v = {8,5,10,6,3,2,7};
+	vector<int> ans = solve(v);
+	for(int x : ans) cout<<x<<" ";
+	cout<<endl;
 	return 0;
 }
--- stacks/NGR.cpp
+++ stacks/NGR.cpp
@@ -10,46 +10,42 @@ pop() untill arr[i] < s.top()
 reverse the ans vector and return it
 */
 #include<iostream>
-#include<stack>
 #include<vector>
+#include<stack>
 #include<algorithm>
-
 using namespace std;
 vector<int> solve(vector<int> v){
-    stack<int> s;
-    vector<int> ans;
-    for(int i = v.size()-1; i>=0; i--){
-        if(s.empty()){
-            ans.push_back(-1);
-        }
-        else if(!s.empty() and s.top()>v[i]){
-            ans.push_back(s.top());
-        }
-        else if(!s.empty() and s.top()<=v[i]){
-            while(s.size()>0 and s.top()<=v[i]){
-                s.pop();
-            }
-            if(s.size()==0){
-                ans.push_back(-1);
-            }
-            else{
-                ans.push_back(s.top());
-            }
-        }
-        s.push(v[i]);
+	
+	vector<int> ans;
+	stack<int> s;
 
-    }
-    reverse(ans.begin(), ans.end());
-    return ans;
+	for(int i = v.size()-1; i>=0; i--){
+		if(s.empty()){
+			ans.push_back(-1);
+		}
+		else if(v[i]<s.top()){
+			ans.push_back(s.top());
+		}
+		else if(v[i] >= s.top()){
+			while(!s.empty() and v[i] >= s.top()){
+				s.pop();
+			}
+			if(s.empty()){
+				ans.push_back(-1);
+			}
+			else{
+				ans.push_back(s.top());
+			}
+		}
+		s.push(v[i]);
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
 }
-int main()
-{
-    vector<int> v = {1,3,2,4};
-    vector<int> ans;
-    ans = solve(v);
-    for(int x: ans){
-        cout<<x<<", ";
-    }
-    cout<<endl;    
-    return 0;
+int main(){
+	vector<int> v = {8,5,10,6,3,2,7};
+	vector<int> ans = solve(v);
+	for(int x : ans) cout<<x<<" ";
+	cout<<endl;
+	return 0;
 }
