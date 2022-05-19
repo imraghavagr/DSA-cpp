ca114338befd294157e9a4d6d14d761512a1b4ce
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-05-19 09:08:44 +0530

    some changes

--- practice.cpp
+++ practice.cpp
@@ -1,47 +1,32 @@
 #include<iostream>
-#include<vector>
 #include<string>
-#include<algorithm>
+#include<stack>
 using namespace std;
-vector<string> dictWords(string textInput){
-	// latt matter cat matter cat
-	//cat batman
-	//0123456789
-	vector<string> ans;
-	string s = "";
-	string s2 = "";
 
-	for(int i = 0; i<textInput.length(); i++){
-		while(!isspace(textInput[i]) and i<textInput.length()){
-			s = s + textInput[i];
-			//cout<<s<<" ";
+string reverseOnlyLetters(string s) {
+	int i = 0; 
+	int j = s.length()-1;
+	if(s.length() == 1)	 return s;
+
+	while(i<=j){
+		if(isalpha(s[i]) and isalpha(s[j])){
+			swap(s[i],s[j]);
+			i++;
+			j--;
+		}
+		if(!isalpha(s[i])){
 			i++;
 		}
-		int idx = s2.find(s);
-		if(idx != -1){
-			ans.push_back(s);
-		}
-		s2 = s2 + " ";
-		s2 = s2 + s;
-		s = "";
-	}
-	sort(ans.begin(),ans.end());
-	for(int i = 1; i<ans.size(); i++){
-		if(ans[i] == ans[i-1]){
-			ans.erase(ans.begin()+i,ans.begin()+i+1);
+		if(!isalpha(s[j])){
+			j--;
 		}
 	}
-	return ans;
+	return s;
 }
 
 int main(){
-	string textInput;
-	getline(cin,textInput);
-
-	vector<string> ans = dictWords(textInput);
-	for(int i = 0; i<ans.size(); i++){
-		cout<<ans[i]<<" ";
-	}
-
+	string s = "a-bC-dEf-ghIj" ;
+	s = reverseOnlyLetters(s);
+	cout<<s<<endl;
 	return 0;
 }
