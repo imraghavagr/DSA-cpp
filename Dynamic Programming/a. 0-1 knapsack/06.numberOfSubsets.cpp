88b4de7194b9fb193106d05971abc46551d22abf
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-06-02 17:43:12 +0530

    some changes

--- Dynamic Programming/a. 0-1 knapsack/06.numberOfSubsets.cpp
+++ Dynamic Programming/a. 0-1 knapsack/06.numberOfSubsets.cpp
@@ -1,36 +1,35 @@
-//https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1/
-//given an array, and a sum , return true if there exists a subset of the array whose sum is equal to given sum value.
-//T= O(sum*N)
+//given an array of natural numbers and a sum value x;
+//find the number of subsets of arr with sum equal to x.
+//direct varation of subset sum problem
+//0 is not present in the array
 #include<iostream>
-#include<vector>
 using namespace std;
-
-bool isSubsetSumTabular(vector<int>arr, int sum){
+int findNumSubsets(int* arr, int n, int sum){
     // code here 
     
     //declaring the matrix
-    int  t[101][100001];
+    int  t[n+1][sum+1];
     //initialization
-    for(int i = 0; i<arr.size()+1; i++){
+    for(int i = 0; i<n+1; i++){
         for(int j= 0; j<sum+1; j++){
             if(i==0){
-                t[i][j] = false;
+                t[i][j] = 0;
             }
             if(j==0){
-                t[i][j]= true;
+                t[i][j]= 1;
             }
         }
     }
     
     //filling the remaining table
     
-    for(int i =1; i<arr.size()+1; i++){
+    for(int i =1; i<n+1; i++){
         for(int j= 1; j<sum+1; j++){
             
             //element is smaller than or equal to sum
             if(arr[i-1]<=j){
                 //two choices- either take it or do not take it
-                t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
             }
             else{
                 //element is greater than sum 
@@ -40,13 +39,14 @@ bool isSubsetSumTabular(vector<int>arr, int sum){
         }
     }
     
-    return t[arr.size()][sum];
+    return t[n][sum];
 }
 
 int main()
 {
-    vector<int> v = {3,34,4,12,5,2}; //ans = yes/1
-    int sum = 9;
-    cout<<isSubsetSumTabular(v,sum)<<endl;
+    int arr[] = {2,3,5,6,8,10};
+    int n = sizeof(arr)/sizeof(int);
+    int sum = 10;
+    cout<<findNumSubsets(arr,n,sum)<<endl;//3 for this input
     return 0;
 }
