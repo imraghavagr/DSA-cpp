0911a68ebcdfe8982f793fac436e23cfc266701e
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-06-08 19:24:27 +0530

    add 06*.cpp

---
+++ Dynamic Programming/c.LongestCommonSubsequenceLCI/06.shortestCommonSupersequence.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int LCS(string a, string b, int m, int n){
+    int t[m+1][n+1];
+    //intitialization
+    for(int i=0; i<m+1; i++){
+        for(int j =0; j<n+1; j++){
+            if(i==0 || j==0){
+                t[i][j] = 0;
+            }
+        }
+    }
+
+    //
+    for(int i=1; i<m+1; i++){
+        for(int j =1; j<n+1; j++){
+            if(a[i-1]==b[j-1]){
+                t[i][j] = 1+ t[i-1][j-1];
+            }
+            else{
+                t[i][j] = max(t[i-1][j],t[i][j-1]);
+            }
+        }
+    }
+    return t[m][n];
+}
+int shortestSupersequence(string a,string b, int m, int n){
+    int lenLCS = LCS(a,b,m,n);
+    return m+n-lenLCS;
+}
+int main()
+{       
+    string a;
+    string b;
+    cin>>a>>b;
+    int m = a.length();
+    int n = b.length();
+    cout<<shortestSupersequence(a,b,m,n)<<endl;
+    return 0;
+}
