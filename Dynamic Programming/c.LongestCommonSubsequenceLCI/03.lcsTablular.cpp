19ee3002fb62c1c220cc3ab03d351c5a898a8ad2
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-06-08 11:30:18 +0530

    add 03*.cpp

---
+++ Dynamic Programming/c.LongestCommonSubsequenceLCI/03.lcsTablular.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/longest-common-subsequence/submissions/
+//https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1#
+#include<iostream>
+using namespace std;
+int longestCommonSubsequence(string text1, string text2) {
+    //let m,n be the size of text1 and text2 respectively
+    int m = text1.length();
+    int n = text2.length();
+    
+    int t[m+1][n+1];
+    
+    for(int i = 0; i<m+1; i++){
+        for(int j =0; j<n+1; j++){
+            if(i==0||j==0){
+                t[i][j] = 0;
+            }
+        }
+    }
+    for(int i = 1; i<m+1; i++){
+        for(int j =1; j<n+1; j++){
+            if(text1[i-1]==text2[j-1]){
+                t[i][j] = 1 + t[i-1][j-1];
+            }
+            else{
+                t[i][j] = max(t[i-1][j],t[i][j-1]);
+            }
+        }
+    }
+    return t[m][n];
+    }
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<longestCommonSubsequence(s1,s2)<<endl;
+    return 0;
+}
