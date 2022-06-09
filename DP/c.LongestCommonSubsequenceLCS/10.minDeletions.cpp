d1dd80543c037267480bf61dc879bfcc8a7cdccd
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-06-10 00:06:44 +0530

    add 10*.cpp

---
+++ DP/c.LongestCommonSubsequenceLCS/10.minDeletions.cpp
@@ -0,0 +1,45 @@
+//given a string s, return the min number of deletions required to make it a pallindrome.
+//https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions4610/0/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int LCS(string a, string b, int n){
+    int t[n+1][n+1];
+    for(int j = 0; j<n+1; j++){
+        t[0][j] = 0;
+    }
+    for(int i = 1; i<n+1; i++){
+        t[i][0] = 0;
+    }
+    
+    for(int i =1; i<n+1; i++){
+        for(int j = 1; j<n+1; j++){
+            if(a[i-1]==b[j-1]){
+                t[i][j] = 1 + t[i-1][j-1];
+            }
+            else{
+                t[i][j] = max(t[i-1][j],t[i][j-1]);
+            }
+        }
+    }
+    return t[n][n];
+}
+int LPS(string a,int n){
+    string b = a;
+    reverse(b.begin(),b.end());
+    return LCS(a,b,n);
+}
+int minDeletions(string str, int n) { 
+    //complete the function here 
+    //min number of deletions = len(str)-len(longest pallindromic subsequence)
+    int lps = LPS(str,n);
+    return n-lps;
+} 
+int main()
+{   
+    string s;
+    cin>>s;
+    int n = s.length();
+    cout<<minDeletions(s,n)<<endl;
+    return 0;
+}
