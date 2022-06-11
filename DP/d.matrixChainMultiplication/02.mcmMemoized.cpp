0ea1841e808d2f7b388b6d1843eab20430991b11
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-06-11 15:51:33 +0530

    add 02*.cpp

---
+++ DP/d.matrixChainMultiplication/02.mcmMemoized.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<climits>
+#include<cstring>
+using namespace std;
+
+int t[101][101];
+
+int solve(int arr[], int i, int j){
+    if(i>=j){
+        return 0;
+    }
+    if(t[i][j] != -1)   return t[i][j];
+    int ans = INT_MAX;
+    for(int k = i;k<=j-1; k++){
+        int tempans = solve(arr,i,k)+solve(arr,k+1,j) + (arr[i-1]*arr[k]*arr[j]);
+        ans = min(ans,tempans);
+    }
+    return t[i][j] = ans;
+}
+int matrixMultiplication(int n, int arr[])
+{
+    // i = 1, j = n-1
+    memset(t,-1,sizeof(t));
+    return solve(arr,1,n-1);
+}
+int main()
+{   
+    int arr[100];
+    int n;
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<matrixMultiplication(n,arr)<<endl;
+    return 0;
+}
