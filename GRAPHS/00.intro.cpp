ccc5984d92723055c20aa92c5da7e360cf44a579
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-10-17 18:37:15 +0530

    add intro to graphs

---
+++ GRAPHS/00.intro.cpp
@@ -0,0 +1,16 @@
+/*
+Input format- 
+5 6 - n-no. of nodes and m - no. of edges
+2 1 - signifies that there is a an edge between node 2 and 1 .. also vice-versa (in case of undirected graph)
+1 3
+2 4
+3 4
+3 5
+4 5
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
