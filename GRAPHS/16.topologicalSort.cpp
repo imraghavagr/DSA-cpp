61bc0cd5ad809489f434ffa048f4dbc2e1232ca0
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-11-30 16:26:09 +0530

    add 16*.cpp

---
+++ GRAPHS/16.topologicalSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+void dfs(int node,vector<int> &visArr, vector<int> adj[], stack<int>&stk){
+    //mark the current node as visited
+    visArr[node] = 1;
+    
+    for(int n : adj[node]){
+        if(!visArr[n]){
+            dfs(n,visArr,adj,stk);
+        }
+    }
+    stk.push(node);
+}
+//Function to return list containing vertices in Topological order. 
+vector<int> topoSort(int V, vector<int> adj[]) 
+{
+    vector<int> ans;
+    vector<int> visArr(V,0);
+    stack<int> stk;
+    
+    for(int i = 0; i<V; i++){
+        if(!visArr[i]){
+            dfs(i,visArr,adj,stk);
+        }
+    }
+    
+    while(!stk.empty()){
+        ans.push_back(stk.top());
+        stk.pop();
+    }
+    return ans;
+}
+int main()
+{
+    return 0;
+}
