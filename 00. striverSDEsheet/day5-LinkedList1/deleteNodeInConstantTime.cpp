61c2be46a8a326c9e6c83716e6a076c64d304bed
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-03-25 12:38:48 +0530

    changes

---
+++ 00. striverSDEsheet/day5-LinkedList1/deleteNodeInConstantTime.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void deleteNode(ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
+int main()
+{
+    return 0;
+}
