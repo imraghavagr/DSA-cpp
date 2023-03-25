198420f069e62a83af2664237ab7a5f453ae88dd
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-03-25 08:21:35 +0530

    changes

--- 00. striverSDEsheet/day1-Arrays1/sortColors.cpp
+++ 00. striverSDEsheet/day1-Arrays1/sortColors.cpp
@@ -30,7 +30,25 @@ void sortColors(vector<int>& nums) {
 
 //apporach 3 - maintain 3 pointers - low, med, high
 //This problem is a variation of the popular Dutch National flag algorithm 
-
+void sortColors(vector<int>& nums) {
+    int low = 0; 
+    int mid = 0;
+    int high = nums.size()-1;
+    while(mid<=high){
+        if(nums[mid] == 0){
+            swap(nums[mid], nums[low]);
+            mid++;
+            low++;
+        }
+        else if(nums[mid] == 1){
+            mid++;
+        }
+        else{  
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
 
 int main()
 {
