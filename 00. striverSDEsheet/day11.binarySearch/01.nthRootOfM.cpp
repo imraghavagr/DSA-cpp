124fc43e924aaa6571b93f7d7c9529c1c60fd4c6
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-04-03 12:51:00 +0530

    changes

---
+++ 00. striverSDEsheet/day11.binarySearch/01.nthRootOfM.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+double mutliply(double number, int n){
+    double ans = 1.0;
+    for(int i = 1; i<=n; i++){
+        ans = ans * number;
+    }
+    return ans;
+}
+double getNthRoot(int n, int m){
+    double low = 1;
+    // double high = m/n; we can also reduce the search space by doing this
+    double high = m;
+    //epsilon - difference.. keep 1 more than the number of places we want our ans
+    double eps = 1e-6;
+
+    while((high - low) > eps){
+        double mid = low + (high-low)/2;
+        if(mutliply(mid, n) < m){
+            low = mid;
+        }
+        else{
+            high = mid;
+        }
+    }
+    cout<<low<<" "<<high<<endl;
+    //jus to check 
+    cout<<pow(m, (double)(1.0/(double)n))<<endl;
+    
+    //we can also return high
+    return low;
+}
+int main()
+{   
+    int n,m;
+    cin>>n>>m;
+    cout<<getNthRoot(n,m)<<endl;
+    return 0;
+}
