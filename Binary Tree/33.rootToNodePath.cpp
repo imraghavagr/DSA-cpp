434185560331b3778c61bd7d01cf48c866b269fb
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-04-28 08:36:38 +0530

    add code for root to node path

--- Binary Tree/33.rootToNodePath.cpp
+++ Binary Tree/33.rootToNodePath.cpp
@@ -1,5 +1,26 @@
+//https://www.interviewbit.com/problems/path-to-given-node/
 #include<iostream>
 using namespace std;
+bool preOrderTraversal(TreeNode* node, int B, vector<int>& ans){
+    if(node == NULL)   return false;
+    
+    //root, left, right 
+    ans.push_back(node->val);
+    
+    if(node->val == B)  return true;
+    
+    if(preOrderTraversal(node->left, B, ans) || preOrderTraversal(node->right, B, ans))
+        return true;
+    
+    ans.pop_back();
+    return false;
+}
+vector<int> Solution::solve(TreeNode* A, int B) {
+    if(A->val == B) return {B};    
+    vector<int> ans;    
+    bool dummy = preOrderTraversal(A, B, ans);
+    return ans;
+}
 int main()
 {
     return 0;
