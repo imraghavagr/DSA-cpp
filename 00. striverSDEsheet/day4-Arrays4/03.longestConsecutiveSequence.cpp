44dde5a6e18cf8823916379764d95aaf46aaa200
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-05-09 17:05:37 +0530

    changes

--- 00. striverSDEsheet/day4-Arrays4/03.longestConsecutiveSequence.cpp
+++ 00. striverSDEsheet/day4-Arrays4/03.longestConsecutiveSequence.cpp
@@ -1,5 +1,40 @@
 #include<iostream>
 using namespace std;
+//using set ds - O(nlogx) time, O(x) space .. x size of set, which will be equal to n in the worst case when all elements are distinct
+int longestConsecutive1(vector<int>& nums) {
+    set<int> st(nums.begin(), nums.end());
+    int prev = INT_MIN;
+    int longest = 0;
+    int cnt = 0;
+    for(auto it : st){
+        if(it == prev+1)    cnt++;
+        else{
+            cnt = 1;
+        }
+        prev = it;
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+//Sorting - O(nlogn)time O(1)space
+int longestConsecutive2(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0, longest = 0;
+    for(int num : nums){
+        if(lastSmaller+1 == num){
+            cnt++;
+            lastSmaller = num;
+        }
+        else if(lastSmaller != num){
+            cnt = 1;
+            lastSmaller = num;
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+//Optimal method - O(n) time and O(1) space
 int main()
 {
     return 0;
--- GRAPHS/12.numberOfDistinctIslands.cpp
+++ GRAPHS/12.numberOfDistinctIslands.cpp
@@ -3,59 +3,88 @@
 #include<set>
 using namespace std;
 
-void bfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &visArr, set<vector<pair<int,int>>> &st, int m, int n){
+vector<int> delR = {-1, 0, 1, 0};
+vector<int> delC = {0, 1, 0, -1};
+bool isValid(int i, int j, int m, int n){
+    if(i>=0 && i<m && j>=0 && j<n)  return true;
+    else    return false;
+}
+
+//using bfs
+void bfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &visArr,
+vector<pair<int,int>> &v, int m, int n, int baseI, int baseJ){
+
     queue<pair<int,int>> q;
-    q.push(make_pair(i,j));
+    q.push({i,j});
     visArr[i][j] = 1;
-    //this i,j is the base of the island 
-    //add this into the set
-    vector<pair<int,int>> v;
-    int baseI = i;
-    int baseJ = j;
-    v.push_back({i-baseI,j-baseJ});
-    //now traverse all the unvisited valid neighbours
     while(!q.empty()){
-        int i = q.front().first;
-        int j = q.front().second;
+        auto it = q.front();
         q.pop();
-        
-        //left and right
-        for(int k = -1; k <= 1; k++){
-            if(j+k>=0 && j+k<n && !visArr[i][j+k] && grid[i][j+k] == 1){
-                q.push(make_pair(i,j+k));
-                v.push_back({i-baseI,(j+k)-baseJ});
-                visArr[i][j+k] = 1;
-            }
-        }
-        //up and down
-        for(int k = -1; k <= 1; k++){
-            if(i+k>=0 && i+k<m && !visArr[i+k][j] && grid[i+k][j] == 1){
-                q.push(make_pair(i+k,j));
-                v.push_back({(i+k)-baseI, j-baseJ});
-                visArr[i+k][j] = 1;
+        i = it.first;
+        j = it.second;
+        for(int k = 0; k<4; k++){
+            int newR = i + delR[k];
+            int newC = j + delC[k];
+            if(isValid(newR, newC, m, n) && !visArr[newR][newC] && grid[newR][newC] == 1){
+                visArr[newR][newC] = 1;
+                v.push_back({newR-baseI, newC-baseJ});
+                q.push({newR, newC});
             }
         }
     }
-    st.insert(v);
-    
+
 }
-int countDistinctIslands(vector<vector<int>>& grid) {
-    int m = grid.size();
-    int n = grid[0].size();
-    
-    vector<vector<int>> visArr(m,vector<int>(n,0));
-    
+int numDistinctIslands(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> visArr(m, vector<int>(n, 0));
     set<vector<pair<int,int>>> st;
-    //lets use the bfs traversal technique
+    
     for(int i = 0; i<m; i++){
-        for(int j =0; j<n; j++){
+        for(int j = 0; j<n; j++){
+            vector<pair<int,int>> v;
             if(!visArr[i][j] && grid[i][j] == 1){
-                bfs(i,j,grid,visArr,st, m, n);
+                v.push_back({i-i,j-j});
+                //we will also pass the baseI and baseJ as i and i
+                bfs(i,j,grid,visArr,v, m, n, i, j);
+                st.insert(v);
             }
         }
     }
     return st.size();
-    // code here
+}
+
+//using dfs
+void dfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &visArr,
+vector<pair<int,int>> &v, int m, int n, int baseI, int baseJ){
+
+    visArr[i][j] = 1;
+    // v.push_back({i-baseI,j-baseJ});
+    for(int k = 0; k<4; k++){
+        int newR = i + delR[k];
+        int newC = j + delC[k];
+        if(isValid(newR, newC, m, n) && !visArr[newR][newC] && grid[newR][newC] == 1){
+            v.push_back({newR-baseI, newC-baseJ});
+            dfs(newR, newC, grid, visArr, v, m, n, baseI, baseJ);
+        }
+    }
+}
+int numDistinctIslands(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> visArr(m, vector<int>(n, 0));
+    set<vector<pair<int,int>>> st;
+    
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            vector<pair<int,int>> v;
+            if(!visArr[i][j] && grid[i][j] == 1){
+                v.push_back({i-i,j-j});
+                //we will also pass the baseI and baseJ as i and i
+                dfs(i,j,grid,visArr,v, m, n, i, j);
+                st.insert(v);
+            }
+        }
+    }
+    return st.size();
 }
 int main()
 {
