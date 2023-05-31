52bc0ed5e743d44786da4efdb21bebbafc2b3f94
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-05-31 11:33:36 +0530

    changes

--- DP/04.Unbounded Knapsack/04.coinChange2.cpp
+++ DP/04.Unbounded Knapsack/04.coinChange2.cpp
@@ -2,28 +2,79 @@
 #include<iostream>
 #include<climits>
 using namespace std;
-int coinChange(vector<int>& coins, int amount) {
-    
-    int n = coins.size();
-    int t[n+1][amount+1];  
+//recursion + memoisation
+int solve(int n, int amount, vector<int> &coins, vector<vector<int>> &dp){
+    //if there is no amount left, but coins are remaining, then there is no way to make that change 
+    if(amount == 0) return 0;
+    //if there are no coins left, but some sum is left, than this can't be possible, so we return..
+    //a large value s.t. it does not gets counted while calculation min 
+    if(n == 0)  return 1e8;
 
-    for(int j=0; j <= amount; j++)
-        t[0][j] = INT_MAX-1;
-    for(int i=0; i <= n; i++)
-        t[i][0] = 0;
-    
-    for(int i=1; i < n+1; i++){
-        for(int j=1; j < amount+1; j++) {
-            if(coins[i-1] <= j)
-                t[i][j] = min(1 + t[i][j-coins[i-1]], t[i-1][j]);
-            else
-                t[i][j] = t[i-1][j];
+    if(dp[n][amount] != -1) return dp[n][amount];
+    if(coins[n-1] <= amount){
+        int take = 1 + solve(n, amount-coins[n-1], coins, dp);
+        int notTake = solve(n-1, amount, coins, dp);
+        return dp[n][amount] = min(take, notTake);
+    }
+    else{
+        int notTake = solve(n-1, amount, coins, dp);
+        return dp[n][amount] = notTake;
+    }
+}
+int coinChange(vector<int>& coins, int amount) {
+    vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, -1));
+    int ans = solve(coins.size(), amount, coins, dp);
+    if(ans == 1e8)   return -1;
+    else return ans;
+}
+//tabular approach 
+int coinChange(vector<int>& coins, int amount) {
+    vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0));
+
+    for(int j = 0; j<=amount; j++){
+        dp[0][j] = 1e8;
+    }       
+    for(int i = 0; i<=coins.size(); i++){
+        dp[i][0] = 0;
+    }
+
+    for(int i = 1; i<=coins.size(); i++){
+        for(int j = 1; j<=amount; j++){
+            if(coins[i-1] <= j){
+                int take = 1 + dp[i][j-coins[i-1]];
+                int notTake = dp[i-1][j];
+                dp[i][j] = min(take, notTake);
+            }
+            else{
+                int notTake = dp[i-1][j];
+                dp[i][j] = notTake;
+            }
         }
     }
-    
-    //Return the fewest number of coins that you need to make up that amount. 
-    //If that amount of money cannot be made up by any combination of the coins, return -1.
-    return t[n][amount] > amount ? -1 : t[n][amount]; 
+    int ans = dp[coins.size()][amount];
+    if(ans == 1e8)   return -1;
+    else return ans;
+}
+
+//space optimised tabular approach 
+int coinChange(vector<int>& coins, int amount) {
+
+    vector<int> prev(amount+1, 1e8);
+    vector<int> curr(amount+1, 0);
+    for(int i = 1; i<=coins.size(); i++){
+        for(int j = 1; j<=amount; j++){
+            if(coins[i-1] <= j){
+                curr[j] = min(1 + curr[j-coins[i-1]], prev[j]);
+            }
+            else{
+                curr[j] = prev[j];
+            }
+        }
+        prev = curr;
+    }
+    int ans = prev[amount];
+    if(ans == 1e8)   return -1;
+    else return ans;
 }
 int main()
 {   
