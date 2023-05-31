47c6fa0272220e2df2e5624d17ae7ce60906de8e
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-05-31 10:50:53 +0530

    changes

--- DP/04.Unbounded Knapsack/03.coinChange1.cpp
+++ DP/04.Unbounded Knapsack/03.coinChange1.cpp
@@ -1,43 +1,63 @@
-/* Same as numberOfSubsets ques. Just add unbounded property.
-https://practice.geeksforgeeks.org/problems/coin-change2448/1#
-*/
+/* Same as numberOfSubsets ques. Just add unbounded property.*/
+
 #include<iostream>
 using namespace std;
-long long int count(int arr[], int n, int sum) {
-    // code here.
-    //declaring the matrix
-    long long int t[n+1][sum+1];
-    //initialization
-    for(int i = 0; i<n+1; i++){
-        for(int j= 0; j<sum+1; j++){
-            if(i==0){
-                t[i][j] = 0;
-            }
-            if(j==0){
-                t[i][j]= 1;
-            }
-        }
+
+//recursion + memoization 
+int solve(int n, int amount, vector<int> &coins, vector<vector<int>> &dp){
+    if(amount == 0) return 1;
+    if(n == 0)  return 0;
+    if(dp[n][amount] != -1) return dp[n][amount];
+    if(coins[n-1] <= amount){
+        return dp[n][amount] = solve(n, amount-coins[n-1], coins, dp) + solve(n-1, amount, coins, dp);
     }
-    
-    //filling the remaining table
-    
-    for(int i =1; i<n+1; i++){
-        for(int j= 1; j<sum+1; j++){
-            
-            //element is smaller than or equal to sum
-            if(arr[i-1]<=j){
-                //two choices- either take it or do not take it
-                t[i][j] = t[i][j-arr[i-1]] + t[i-1][j];
+    else{
+        return dp[n][amount] = solve(n-1, amount, coins, dp);
+    }
+}
+int change(int amount, vector<int>& coins) {
+    vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, -1));
+    return solve(coins.size(), amount, coins, dp);
+}
+
+//tabular approach
+int change(int amount, vector<int>& coins) {
+    vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0));
+    for(int i = 0; i<=coins.size(); i++){
+        dp[i][0] = 1;
+    }
+    for(int i = 1; i<=coins.size(); i++){
+        for(int j = 1; j<=amount; j++){
+            if(coins[i-1] <= j){
+                dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
             }
             else{
-                //element is greater than sum 
-                //do not take it    
-                t[i][j] = t[i-1][j];
+                dp[i][j] = dp[i-1][j];
             }
         }
     }
+    return dp[coins.size()][amount];
+}
 
-    return t[n][sum];
+//space optimised tabular approach 
+int change(int amount, vector<int>& coins) {        
+    vector<int> prev(amount+1, 0);
+    vector<int> curr(amount+1, 0);
+
+    prev[0] = curr[0] = 1;
+
+    for(int i = 1; i<=coins.size(); i++){
+        for(int j = 1; j<=amount; j++){
+            if(coins[i-1] <= j){
+                curr[j] = curr[j-coins[i-1]] + prev[j];
+            }
+            else{
+                curr[j] = prev[j];
+            }
+        }
+        prev = curr;
+    }
+    return prev[amount];
 }
 int main()
 {
