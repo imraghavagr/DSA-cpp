c3bd9aa97db90a96daa66cd452ce01e704adc976
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-06-06 19:59:57 +0530

    changes

---
+++ 00. striverSDEsheet/day4-Arrays4/04.longestSubarrayWithKSum.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+//brute force - O(n^2) time, O(1) space
+int maxLen(vector<int>&A, int n)
+{   
+    int longestLen = 0;
+    for(int i = 0; i<n; i++){
+        int sum = 0;
+        for(int j = i; j<n; j++){
+            sum += A[j];
+            if(sum == 0){
+                longestLen = max(longestLen, (j-i+1));
+            }
+        }
+    }
+    return longestLen;
+}
+
+//also works with negatives and zeros in the array 
+//Better solution using hashing - O(n) time, O(n) space : given we use unordered_map and there are no collisions 
+int getLongestSubarray(vector<int>& nums, int k){
+    long long sum = 0;
+    unordered_map<long long,int> mp;
+    int maxLen = 0;
+    for(int i = 0; i<nums.size(); i++){
+        sum += nums[i];
+        if(sum == k){
+            maxLen = max(maxLen,i+1);
+        }
+        if(mp.find(sum-k) != mp.end()){
+            maxLen = max(maxLen, i-mp[sum-k]);
+        }
+        //mp me sum ki value agar nahi hogi to hii daalenge .. update nahi karenge 
+        //because we want to find the left farthest point where we are getting sum-k
+        if(mp.find(sum) == mp.end())    mp[sum] = i;
+    }
+    return maxLen;
+}
+
+//Only works for all positive elements 
+//sliding window or two pointer approach 
+
+int main()
+{
+    return 0;
+}
