beab817edfe90f5055676ba5092a5f360434a99f
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-06-17 18:48:44 +0530

    changes

--- 00. striverSDEsheet/day11.binarySearch/04.searchElementInSortedRotatedArr.cpp
+++ 00. striverSDEsheet/day11.binarySearch/04.searchElementInSortedRotatedArr.cpp
@@ -1,6 +1,8 @@
 //https://leetcode.com/problems/search-in-rotated-sorted-array/
 #include<iostream>
 using namespace std;
+
+//O(2logn)
 int search(vector<int> &nums, int s, int e, int target){
     while(s<=e){
         int mid = s + (e-s)/2;
@@ -43,6 +45,32 @@ int search(vector<int>& nums, int target) {
     int idx = search(nums, 0, pivot-1, target);
     if(idx != -1)   return idx;
     return search(nums, pivot, nums.size()-1, target);
+}
+
+
+//O(logn)
+int helperBS(vector<vector<int>> &matrix, int m, int n, int mid){
+    //search for cnt of elements <=mid in each row 
+    int cnt = 0; 
+    for(int i = 0; i<m; i++){
+        cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+    }
+    return cnt;
+}
+int getMedian(vector<vector<int>> &matrix)
+{
+    // Write your code here.
+    int s = 1, e = 1e5; 
+    int m = matrix.size(); 
+    int n = matrix[0].size();
+    while(s<=e){
+        int mid = s + (e-s)/2; 
+        //cnt the number of elements <= mid 
+        int cnt = helperBS(matrix, m, n, mid);
+        if(cnt > (m*n)/2)   e = mid - 1;
+        else s = mid + 1;
+    }
+    return s; 
 }
 int main()
 {
