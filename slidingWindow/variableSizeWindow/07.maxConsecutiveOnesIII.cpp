9a9640f4d6880f35003b687d64dec25ceb49e9a6
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-06-26 10:51:56 +0530

    changes

--- slidingWindow/variableSizeWindow/07.maxConsecutiveOnesIII.cpp
+++ slidingWindow/variableSizeWindow/07.maxConsecutiveOnesIII.cpp
@@ -1,18 +1,17 @@
-//https://leetcode.com/problems/max-consecutive-ones/description/
 #include<iostream>
 using namespace std;
-int findMaxConsecutiveOnes(vector<int>& nums) {
+int longestOnes(vector<int>& nums, int k) {
     int i = 0, j = 0; 
     int numZeros = 0; 
     int ans = 0; 
     while(j<nums.size()){
         if(nums[j] == 0)    numZeros++;
-        if(numZeros == 0){
+        if(numZeros <= k){
             ans = max(ans, j-i+1);
             j++;
         }
         else{
-            while(numZeros > 0){
+            while(numZeros > k){
                 if(nums[i] == 0)    numZeros--;
                 i++;
             }
