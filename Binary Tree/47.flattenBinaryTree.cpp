2a5a89d4ced07adb01330fa5e498a71ed37019df
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-07-04 19:47:06 +0530

    changes

--- Binary Tree/47.flattenBinaryTree.cpp
+++ Binary Tree/47.flattenBinaryTree.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-//Approach 1 => Recursive solution:
+//Approach 1 => Recursive solution:O(n) time, O(n) space
 //             1. do right left root traversal
 //             2. For curr node, update curr->right = prev; curr->left = NULL; 
 //             3. Update prev = curr;
@@ -25,7 +25,7 @@ void flatten(TreeNode* root) {
     solve(root);
 }
 
-//Iterative method - using stack 
+//Iterative method - using stack -> O(n) time, O(n) space
 void flatten(TreeNode* root) {
     if(root == NULL)    return;
     stack<TreeNode*> stk; 
@@ -44,6 +44,30 @@ void flatten(TreeNode* root) {
             curr->right = stk.top();
         }
         curr->left = NULL;
+    }
+}
+
+//Optimal Approach -> O(n) time, O(1) space
+//Similar to morris traversal 
+void flatten(TreeNode* root) {
+    if(root == NULL)    return; 
+    TreeNode* curr = root; 
+    while(curr != NULL){
+        if(curr->left != NULL){
+            //first find the rightmost node in the ls of curr node 
+            TreeNode* prev = curr->left; 
+            while(prev->right != NULL){
+                prev = prev->right; 
+            }
+            //now connect that last node to the curr node's right 
+            prev->right = curr->right; 
+            //also connect curr's right to curr's left 
+            curr->right = curr->left; 
+            //also make the current nodes left as null 
+            curr->left = NULL; 
+        }
+        //move curr to curr->right 
+        curr = curr->right; 
     }
 }
 int main()
