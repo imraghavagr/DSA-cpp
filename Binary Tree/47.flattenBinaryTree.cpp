db27678bdd971a72e9037f0a1ba28b25eddd15c7
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-07-04 19:23:59 +0530

    changes

--- Binary Tree/47.flattenBinaryTree.cpp
+++ Binary Tree/47.flattenBinaryTree.cpp
@@ -24,6 +24,28 @@ void flatten(TreeNode* root) {
     if(root == NULL)    return;
     solve(root);
 }
+
+//Iterative method - using stack 
+void flatten(TreeNode* root) {
+    if(root == NULL)    return;
+    stack<TreeNode*> stk; 
+    stk.push(root);
+    while(!stk.empty()){
+        TreeNode* curr = stk.top();     stk.pop(); 
+        if(curr -> right){
+            stk.push(curr->right);
+        }
+        if(curr->left){
+            stk.push(curr->left);
+        }
+        //since we have inserted left after right, because of LIFO, the prev nodes left...
+        //will be at the stk.top .. and we need to connect that to curr nodes right 
+        if(!stk.empty()){
+            curr->right = stk.top();
+        }
+        curr->left = NULL;
+    }
+}
 int main()
 {
     return 0;
