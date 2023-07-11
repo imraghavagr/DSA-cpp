129133e4742f77246af90d36e74b991ce7b1aa6c
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-07-11 09:10:53 +0530

    changes

--- 00. striverSDEsheet/day07-LinkedListAndArrays/02.cloneLL.cpp
+++ 00. striverSDEsheet/day07-LinkedListAndArrays/02.cloneLL.cpp
@@ -20,7 +20,41 @@ Node* copyRandomList(Node* head) {
     return mp[head];
 }
 //optimal approach - without using extra space other than the new linked list created
+Node* copyRandomList(Node* head) {
+    if(head == NULL)    return head; 
+    //step 1: create dummy nodes for every node 
+    Node *temp = head; 
+    Node* newNode;
+    while(temp != NULL){
+        newNode = new Node(temp->val);
+        newNode->next = temp->next;
+        temp->next = newNode; 
+        //update temp
+        //since we have changed temp->next, we will go to the origal 
+        //...next node via newNode created
+        temp = temp->next->next;
+    }
+    //step2 : rearrange the random pointers 
+    temp = head; 
+    while(temp != NULL){
+        if(temp->random != NULL)
+            temp->next->random = temp->random->next; 
+        temp = temp->next->next; 
+    }
 
+    //step3 : get the original linked list and the deep copy linked list 
+    temp = head->next; 
+
+    Node *ptr1 = head, *ptr2 = ptr1->next->next; 
+    while(ptr2 != NULL){
+        ptr1->next->next = ptr2->next; 
+        ptr1->next = ptr2;
+        ptr1 = ptr2; 
+        ptr2 = ptr1->next->next; 
+    }
+    ptr1->next = NULL; 
+    return temp; 
+}
 int main()
 {
     return 0;
