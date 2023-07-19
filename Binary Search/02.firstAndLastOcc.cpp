476889bff769fef8a3a34f7f2e223d72a6fc858e
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-07-19 09:56:22 +0530

    changes

--- Binary Search/02.firstAndLastOcc.cpp
+++ Binary Search/02.firstAndLastOcc.cpp
@@ -1,49 +1,30 @@
 #include<iostream>
 using namespace std;
-int firstOccurence(int *arr, int n, int x){
-    int s = 0, e = n-1; 
-    int ans = -1;
+int findOcc(int target, vector<int> &nums, int flag){
+    int ans = -1; 
+    int s = 0, e = nums.size()-1;
     while(s<=e){
-        int mid = s + (e-s)/2;
-        if(arr[mid] == x){
-            ans = mid; 
-            e = mid-1;  
-        }
-        else if(arr[mid] > x){
-            e = mid - 1;
-        }
-        else{
-            s = mid+1;
-        }
-    }
-    return ans;
-}
-int lastOccurence(int *arr, int n, int x){
-    int s = 0, e = n-1;
-    int ans = -1;
-    while(s<=e){
-        int mid = s + (e-s)/2;
-        if(arr[mid] == x){
+        int mid = s + (e-s)/2; 
+        if(nums[mid] == target){
             ans = mid;
-            s = mid+1;
+            if(flag)   s = mid + 1;
+            else    e = mid - 1;
         }
-        else if(arr[mid] < x){
-            s = mid+1;
-        }
-        else{
-            e = mid-1;
-        }
+        else if(target > nums[mid]) s = mid + 1;
+        else    e = mid - 1;
     }
+    return ans; 
+}
+vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> ans; 
+    int fo = findOcc(target, nums,0);
+    if(fo == -1)    return {-1,-1};
+    int lo = findOcc(target, nums, 1);
+    ans.push_back(fo);
+    ans.push_back(lo);
     return ans;
 }
-vector<int> find(int arr[], int n , int x )
-{
-    // code here
-    int first, last; 
-    first = firstOccurence(arr, n, x);
-    last = lastOccurence(arr, n, x);
-    return {first, last};
-}
+int main()
 {
     return 0;
 }
