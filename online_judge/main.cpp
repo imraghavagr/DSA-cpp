9f42c4d48c709a802133b83bd7145b1dbf90cad4
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-08-26 12:29:47 +0530

    add changes

--- online_judge/main.cpp
+++ online_judge/main.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ull unsigned long long int
+#define ll long long int
 
-//pos - 16 -> 18
-//sum - 16*9 => 144 -> 150 
+//pos - 10 
+//sum - 10*9 => 92
 //tight - 2
-ull dp[18][150][2]; 
-ull g(string &digits, ull pos = 0, ull tight = 1, ull sum = 0){
+ll dp[10][92][2]; 
+ll g(string &digits, ll pos = 0, ll tight = 1, ll sum = 0){
     if(pos == digits.size()){
         return sum; 
     }
 
     if(dp[pos][sum][tight] != -1)   return dp[pos][sum][tight];
 
-    ull ret = 0; 
-    ull upperLimit = (tight == 1) ? digits[pos] - '0': 9;
+    ll ret = 0; 
+    ll upperLimit = (tight == 1) ? digits[pos] - '0': 9;
 
-    for(ull i = 0; i<=upperLimit; i++){
+    for(ll i = 0; i<=upperLimit; i++){
         if(tight == 1){
             if(i == upperLimit) ret += g(digits, pos+1, 1, sum + i); 
             else    ret += g(digits, pos+1, 0, sum + i); 
@@ -35,20 +35,19 @@ int main()
     freopen("output.txt", "w", stdout);
     #endif
 
-    int t;
-    cin>>t; 
-    ull l, r; 
+    ll l, r; 
     string digits;
 
-    while(t--){
+    while(true){
         cin>>l>>r; 
+        if(l == -1 && r == -1)  break;
         digits = to_string(r); 
         memset(dp, -1, sizeof(dp));
-        ull ans2 = g(digits); 
+        ll ans2 = g(digits); 
         memset(dp, -1, sizeof(dp));
         if(l != 0)  l = l - 1; 
         digits = to_string(l); 
-        ull ans1 = g(digits); 
+        ll ans1 = g(digits); 
         cout<< ans2 - ans1<<endl;
     }    
     return 0;
